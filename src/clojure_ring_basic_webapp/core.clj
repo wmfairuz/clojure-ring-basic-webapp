0a20584d686ea8f8a09a9a756ee1f25683929fe1
Fairuz Wan Ismail <fairuz.ismail@maylab.my>
2015-01-22 11:13:34 +0800

    Lein project using default template
    Signed-off-by: Fairuz Wan Ismail <fairuz.ismail@maylab.my>

---
+++ src/clojure_ring_basic_webapp/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-ring-basic-webapp.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clojure_ring_basic_webapp/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-ring-basic-webapp.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-ring-basic-webapp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
